d203ded206a3db8949ae1610db9de5d44930218a
Brett Fitzpatrick <brett.fitzpatrick@ironnetcybersecurity.com>
2023-05-31 09:35:24 -0400

    added version tagging to correlate with traefik release

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # DockerFile
 
 # Base image
-FROM traefik:latest
+FROM traefik:v2.10.1
 
 # Dynamic config file
 COPY ./traefik_dynamic_config.yml /configs/traefik_dynamic_config.yml
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 docker rmi brettfitz/traefik:latest
 
 # Build docker image and tag
-docker build -t brettfitz/traefik .
+docker buildx build --push --platform linux/amd64 -t brettfitz/traefik -t brettfitz/traefik:v2.10.1 .
 
 # Push to Docker Hub repo as latest tag
 docker push brettfitz/traefik:latest
